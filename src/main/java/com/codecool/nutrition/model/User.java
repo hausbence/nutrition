3b3b67926a1326a065be28c7d26d6f0fd5655f6a
hausbence <bence.hausknecht@gmail.com>
2021-01-08 14:35:04 +0100

    security configuration for registration and login

--- src/main/java/com/codecool/nutrition/model/User.java
+++ src/main/java/com/codecool/nutrition/model/User.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @AllArgsConstructor
 @Builder
@@ -27,6 +29,12 @@ public class User {
 
     @Column(columnDefinition = "text")
     private String email;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     public User(String name, String password, String email) {
         this.name = name;
---
+++ src/main/java/com/codecool/nutrition/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,29 @@
+package com.codecool.nutrition.security.jwt;
+
+import com.codecool.nutrition.security.services.UserDetailsServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
