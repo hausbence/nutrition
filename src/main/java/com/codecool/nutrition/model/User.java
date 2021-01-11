1215e28ac2813cd7bd8a2ceacfd25ff23f393eb2
hausbence <bence.hausknecht@gmail.com>
2021-01-11 09:56:24 +0100

    sql file added for roles, fixed a bug in User constr.

--- src/main/java/com/codecool/nutrition/model/User.java
+++ src/main/java/com/codecool/nutrition/model/User.java
@@ -36,9 +36,9 @@ public class User {
         inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
-    public User(String name, String password, String email) {
-        this.name = name;
-        this.password = password;
+    public User(String username, String email, String password) {
+        this.name = username;
         this.email = email;
+        this.password = password;
     }
 }
