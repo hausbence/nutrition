e2be619f278d6000799dcbf9eb2b575f99c9ed00
hausbence <bence.hausknecht@gmail.com>
2021-03-19 11:28:38 +0100

    system.prop file added

--- src/main/java/com/codecool/nutrition/controller/NutritionController.java
+++ src/main/java/com/codecool/nutrition/controller/NutritionController.java
@@ -4,6 +4,7 @@ import com.codecool.nutrition.fetch.NutritionFetch;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.UnsupportedEncodingException;
 
 @RestController
+@CrossOrigin
 public class NutritionController {
 
     @Autowired
